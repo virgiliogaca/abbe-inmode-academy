9f0ba307f21530a1154ea3a395fb2a44849a77cd
virgiliogaca <81466302+virgiliogaca@users.noreply.github.com>
2022-07-16 00:04:43 -0500

    Update index.js

--- src/api/googleSheets/index.js
+++ src/api/googleSheets/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export function sendUserInfo (name, lastName, phone, email, state, equipment, isAbbeClient, isDermajalClient) {
-    axios.post(`https://sheet.best/api/sheets/11d74675-6d4b-458d-b6a9-087a82dd6b5c`, {
+    axios.post(``, {
         "Nombre": name,
         "Apellido": lastName,
         "WhatsApp": phone,
